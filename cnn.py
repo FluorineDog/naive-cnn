a4e04f84e6dd60c1bed15e929e2781d0a581bb34
FluorineDog <fluorine@gmail.com>
2018-07-07 16:58:27 +0800

    init

---
+++ cnn.py
@@ -0,0 +1,73 @@
+import tensorflow as tf 
+def network(x, y_, dropout_rate, graph):
+  # apply 64*5*5 kernel, with relu as activation
+  # to bs*28*28*14
+  with graph.as_default():
+    conv_layer1 = tf.layers.conv2d(
+      inputs=x,
+      filters=32,
+      kernel_size=[5, 5],
+      padding='same',
+      activation=tf.nn.relu,
+    )
+
+    # apply 2*2 pooling, nonoverlaped
+    # to bs*14*14*128
+    pool_layer1 = tf.layers.max_pooling2d(
+      inputs=conv_layer1, 
+      pool_size=[2, 2], 
+      strides=2
+    )
+
+    # apply 128*5*5 kernel, with relu as activation, 
+    # to bs*14*14*128
+    conv_layer2 = tf.layers.conv2d(
+      inputs=pool_layer1,
+      filters=64,
+      kernel_size=[5, 5],
+      padding='same',
+      activation=tf.nn.relu,
+    )
+
+    # apply 2*2 pooling, non-overlaped
+    # to bs*7*7*128
+    pool_layer2 = tf.layers.max_pooling2d(
+      inputs=conv_layer2, 
+      pool_size=[2, 2],
+      strides=2,
+    )
+
+    # to bs*(7*7*64)
+    trans = tf.reshape(pool_layer2, [-1, 7*7*64])
+
+    # to bs*1024
+    dense_layer= tf.layers.dense(
+      inputs=trans,
+      units=1024,
+      activation=tf.nn.relu,
+    )
+
+    # add dropout
+    # still bs*1024
+    dropout_layer=tf.layers.dropout(
+      inputs=dense_layer,
+      rate=dropout_rate
+    )
+
+    # linear to bs*10
+    y = tf.layers.dense(
+      inputs=dropout_layer, 
+      units=10
+    )
+
+    # softmax with cross entropy
+    loss = tf.losses.sparse_softmax_cross_entropy(labels=y_, logits=y)
+    return y, loss
+
+def model(x, y_, dropout_rate, graph):
+  with graph.as_default():
+    y, loss = network(x, y_, dropout_rate, graph)
+    # optimize all thing into one
+    train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
+    return y, loss, train_step
+
---
+++ mine.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python3
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import sys
+import tensorflow as tf
+import cnn
+
+
+FLAGS = None
+from tensorflow.examples.tutorials.mnist import input_data
+
+
+
+def main(*argv):
+  data_dir = "/tmp/tensorflow/mnist/input_data"
+  # load dataset
+  mnist = input_data.read_data_sets(data_dir)
+
+  # import data as placeholder 
+  # batch_size * height * width * channels
+  # as bs*28*28*1
+  x = tf.placeholder(tf.float32, [None, 28, 28, 1])
+  # true label
+  y_ = tf.placeholder(tf.int64, [None])
+
+  dropout_rate = tf.placeholder(tf.float32)
+
+  graph = tf.Graph()
+  with graph.as_default():
+    y, loss, train_step = cnn.model(x, y_, dropout_rate, graph=graph)
+
+  with tf.Session(graph=graph) as sess:
+    # Train
+    for _ in range(100):
+      for _ in range(100):
+        batch_xs, batch_ys = mnist.train.next_batch(100)
+        sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys, dropout_rate: 0.4})
+
+      # evaluate train model
+      correct_prediction = tf.equal(tf.argmax(y, 1), y_)
+      accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+      print(sess.run(
+          [accuracy, loss], feed_dict={
+              x: mnist.test.images,
+              y_: mnist.test.labels,
+              dropout_rate: 1.0
+          }
+      ))
+
+    # Test trained model
+    # correct_prediction = tf.equal(tf.argmax(y, 1), y_)
+    # accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    # print(sess.run(
+    #     accuracy, feed_dict={
+    #         x: mnist.test.images,
+    #         y_: mnist.test.labels
+    #         dropout_rate: 1.0
+    #     }
+    # ))
+
+
+
+
+
+
+# def run():
+#   parser = argparse.ArgumentParser()
+#   parser.add_argument(
+#       '--data_dir',
+#       type=str,
+#       default='/tmp/tensorflow/mnist/input_data',
+#       help='Directory for storing input data')
+#   FLAGS, unparsed = parser.parse_known_args()
+#   FLAGS.dropout_rate = 0.4
+#   print(FLAGS)
+#   tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
+
+# if __name__ == '__main__':
+#   run()
+
+main()
